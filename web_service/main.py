27bd65a28fb1528ff56edb9015db78a71e353568
Cometores <anicometores@gmail.com>
2023-03-17 16:38:57 +0100

    test

--- web_service/main.py
+++ web_service/main.py
@@ -1,6 +1,6 @@
 from flask import Flask
 
-from spotify_api.spotify_api import SpotifyClient
+#from spotify_api.spotify_api import SpotifyClient
 
 app = Flask(__name__)
 
@@ -10,24 +10,24 @@ app = Flask(__name__)
 def index():
     return "Spotify API"
 
-
-@app.route("/albums_by_artist/<artist_name>")
-def get_albums_by_artist(artist_name: str):
-    albums = spotify_client.get_albums_by_artist(artist_name)
-    return albums
-
-
-@app.route("/tracks_from_album/<album_name>")
-def get_tracks_from_album(album_name: str):
-    tracks = spotify_client.get_tracks_from_album(album_name)
-    return tracks
-
-@app.route("/top_tracks_by_artist/<artist_name>")
-def get_top_tracks_by_artist(artist_name: str):
-    top_tracks = spotify_client.get_top_tracks_by_artist(artist_name)
-    return top_tracks
+#
+# @app.route("/albums_by_artist/<artist_name>")
+# def get_albums_by_artist(artist_name: str):
+#     albums = spotify_client.get_albums_by_artist(artist_name)
+#     return albums
+#
+#
+# @app.route("/tracks_from_album/<album_name>")
+# def get_tracks_from_album(album_name: str):
+#     tracks = spotify_client.get_tracks_from_album(album_name)
+#     return tracks
+#
+# @app.route("/top_tracks_by_artist/<artist_name>")
+# def get_top_tracks_by_artist(artist_name: str):
+#     top_tracks = spotify_client.get_top_tracks_by_artist(artist_name)
+#     return top_tracks
 
 
 if __name__ == "__main__":
-    spotify_client = SpotifyClient()
+    #spotify_client = SpotifyClient()
     app.run(debug=True)
